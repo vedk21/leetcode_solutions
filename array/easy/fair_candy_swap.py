244f26a40da1bbc6638a0aca86925b7721afd596
vedk21 <vedantkakade.cp@gmail.com>
2020-07-26 23:13:01 +0530

    Added: Array - fair candy swaps in two arrays

---
+++ array/easy/fair_candy_swap.py
@@ -0,0 +1,17 @@
+# Problem: Alice and Bob have candy bars of different sizes: A[i] is the size of the i-th bar of candy that Alice has, and B[j] is the size of the j-th bar of candy that Bob has. Since they are friends, they would like to exchange one candy bar each so that after the exchange, they both have the same total amount of candy.  (The total amount of candy a person has is the sum of the sizes of candy bars they have.) Return an integer array ans where ans[0] is the size of the candy bar that Alice must exchange, and ans[1] is the size of the candy bar that Bob must exchange. If there are multiple answers, you may return any one of them.  It is guaranteed an answer exists.
+
+# Sample: A = [1,2,5], B = [2,4], Output: [5,4]
+
+from typing import List
+
+class Solution:
+  def fairCandySwap(self, A: List[int], B: List[int]) -> List[int]:
+    sumA, sumB = sum(A), sum(B)
+    setB = set(B)
+    for x in A:
+      y = x + (sumB - sumA) // 2
+      if y in setB:
+        return [x, y]
+
+solution = Solution()
+print(solution.fairCandySwap([1,2,5], [2,4]))
