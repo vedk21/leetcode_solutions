7afbadc353f1afe62b36a1d2e82e8b6f9dadabbc
vedk21 <vedantkakade.cp@gmail.com>
2022-07-09 00:24:20 +0530

    Added: String - find longest prefix in array of strings

---
+++ string/easy/longest_common_prefix.py
@@ -0,0 +1,41 @@
+#Problem: Write a function to find the longest common prefix string amongst an array of strings.
+
+# Sample: strs = ['car', 'care'], Output: 'car'
+
+from typing import List
+
+class Solution:
+  def longestCommonPrefix(self, strs: List[str]) -> str:
+
+    # find min length string
+    idx, size = self.findMinStr(strs)
+
+    print(idx, size)
+
+    if size:
+      prefix = ''
+      for i in range(size):
+        ch = strs[idx][i]
+        for item in strs:
+          if item[i] != ch:
+            return prefix
+          
+        prefix += ch
+
+      return prefix
+    else:
+      return ''
+
+  def findMinStr(self, strs: List[str]) -> (int, int):
+    size = 210
+    index = 0
+    for i in range(len(strs)):
+      if len(strs[i]) < size:
+        size = len(strs[i])
+        index = i
+    
+    return (index, size)
+
+solution = Solution()
+print(solution.longestCommonPrefix(['car', 'care', 'ascar']))
+    
